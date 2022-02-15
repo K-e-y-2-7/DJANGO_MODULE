36742f8534e946de0e474fa5f80108803552025a
Key27 <Desertod2014@mail.ua>
2022-02-15 08:59:57 +0200

    Добавил модели кастомного пользователя поле кошелёк, класс мета, и функцию __str__

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,23 @@
+import datetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 #Кастомный юзер
 class MyUser(AbstractUser):
-    birth_date = models.DateField()
+    '''Custom user'''
+    birth_date = models.DateField() 
     avatar = models.ImageField(blank=True, null=True)
+    wallet = models.DecimalField(default= 0.0)
+
+    class Meta:
+        ordering = ["first_name, last_name"]
+        verbose_name_plural = "User"
+
+    def __str__(self) -> str:
+        return  f"Username - {self.username}, name - {self.first_name}, \
+surname - {self.last_name}, age - {datetime.timezone.now() - self.birth_date}"
+
+
+
+
 
