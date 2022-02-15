b590dc67e40215b23664acc1c98106b08802737c
Key27 <Desertod2014@mail.ua>
2022-02-15 08:37:34 +0200

    Подключаю своё приложение к джанго. Создаю модель кастомного пользователя и подключаю его в settings.py

--- educational_online_store/settings.py
+++ educational_online_store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'myapp.MyUser' 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+#Кастомный юзер
+class MyUser(AbstractUser):
+    birth_date = models.DateField()
+    avatar = models.ImageField(blank=True, null=True)
+
