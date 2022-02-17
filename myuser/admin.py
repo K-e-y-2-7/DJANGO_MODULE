0eac74cadd6292654d7ffe36e4c8a9a446f3c270
Key27 <Desertod2014@mail.ua>
2022-02-17 03:49:50 +0200

    Немного поправил админку. Провел первую миграцию в приложении myuser

--- myuser/admin.py
+++ myuser/admin.py
@@ -4,8 +4,11 @@ from logging import Filter
 from django.contrib import admin
 import myuser.models
 
+
 @admin.register(myuser.models.MyUser)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'birth_date' 'email', 'is_staff', 'date_joined']
+    list_display = ['username', 'first_name', 'last_name', 'birth_date', 'email', 'is_staff', 'date_joined']
     list_filter = ['username', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined']
+
+#MainAdmin pass: adminpassofdb
 
