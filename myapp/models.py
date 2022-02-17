43aba63d3906bd5abe3681903caaf21805d4c55a
Sergey Gorodilov <sgorodil@gmail.com>
2022-02-17 10:49:13 +0200

    added m2m example

--- myapp/models.py
+++ myapp/models.py
@@ -1,19 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-import django.utils
 
-#Кастомный юзер
+
+# Кастомный юзер
 class MyUser(AbstractUser):
-    birth_date = models.DateField() 
+    birth_date = models.DateField()
     avatar = models.ImageField(blank=True, null=True)
-    wallet = models.DecimalField(default= 0.0, decimal_places=2, max_digits=15)
+    wallet = models.DecimalField(default=0.0, decimal_places=2, max_digits=15)
 
     class Meta:
         ordering = ['first_name', 'last_name']
         verbose_name_plural = 'MyUser'
 
     def __str__(self) -> str:
-        return  f'Name - {self.first_name},surname - {self.last_name}'
+        return f'Name - {self.first_name},surname - {self.last_name}'
 
 
 class Products(models.Model):
@@ -23,7 +23,7 @@ class Products(models.Model):
     quantity_in_stock = models.IntegerField(default=0)
 
     description = models.TextField()
-    
+
     class Meta:
         ordering = ['name', 'price']
         verbose_name_plural = 'Products'
@@ -33,23 +33,29 @@ class Products(models.Model):
 
 
 class Purchases(models.Model):
-    customer = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING, related_name='myuser')
-    product = models.ForeignKey(Products, on_delete=models.DO_NOTHING, related_name='product')
-    count = models.IntegerField(default=1)
-    time = models.DateTimeField(default=django.utils.timezone.now)
+    customer = models.ForeignKey(
+        MyUser, on_delete=models.DO_NOTHING, related_name='myuser'
+    )
+    product = models.ManyToManyField(Products, through="ProductPurchase")
+    time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['customer', 'product']
         verbose_name_plural = 'Purchases'
 
-    def __str__(self):
-        return f'Purchase is successful. Customer: {self.customer},\
-       what buy: {self.product}, count: {self.count}, at time: {self.time}'
+
+class ProductPurchase(models.Model):
+    """Product-Purchase M2M implementation"""
+
+    product = models.ForeignKey(Products, on_delete=models.PROTECT)
+    purchase = models.ForeignKey(Purchases, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
 
 class Returns(models.Model):
-    purchase = models.ForeignKey(Purchases, on_delete=models.CASCADE, related_name='purchases')
-    time = models.DateTimeField(default= django.utils.timezone.now)
+    purchase = models.ForeignKey(
+        Purchases, on_delete=models.CASCADE, related_name='purchases'
+    )
+    time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['purchase', 'time']
