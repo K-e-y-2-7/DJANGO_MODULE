87f815581ccce887775538a1f7bf4e9694d89c2f
Key27 <Desertod2014@mail.ua>
2022-02-17 03:35:34 +0200

    Добавил в админку модель юзера

--- myuser/admin.py
+++ myuser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from logging import Filter
+from django.contrib import admin
+import myuser.models
+
+@admin.register(myuser.models.MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'birth_date' 'email', 'is_staff', 'date_joined']
+    list_filter = ['username', 'first_name', 'last_name', 'email', 'is_staff', 'date_joined']
+
