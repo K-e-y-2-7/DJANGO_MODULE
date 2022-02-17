56ce6472b0d5dbc0d16b7e9b10e4ac64da9c8b46
Key27 <Desertod2014@mail.ua>
2022-02-17 12:15:12 +0200

    Создаю новую базу данных, подключаю к ней существующего юзера и даю ему все права пользования бд. Подключаю бд к джанго в файле settings.py.

--- educational_online_store/settings.py
+++ educational_online_store/settings.py
@@ -77,8 +77,12 @@ AUTH_USER_MODEL = 'myuser.MyUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storedb',
+        'USER': 'mydbuser',
+        'PASSWORD': 'mydbuserpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
